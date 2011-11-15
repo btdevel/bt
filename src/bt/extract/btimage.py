a4f8958a3b952f9e2cb33ac8017755ad29867798
Desmet Irkm <bt@zandere.de>
2011-11-15 11:17:52 +0100

    added ega palette functions

--- src/bt/extract/btimage.py
+++ src/bt/extract/btimage.py
@@ -1,5 +1,5 @@
 import pygame
-import bt.extract.btfile
+import bt.extract.btfile as btfile
 
 
 def pal2rgb(raw_pal, pal):
@@ -8,13 +8,25 @@ def pal2rgb(raw_pal, pal):
         raw_rgb[3 * i:3 * i + 3] = pal[b]
     return raw_rgb
 
-def save_4bit_image(data, size, palette, dest):
-    raw_pal = bytearray(2 * len(data))
+def expand_4bit(data):
+    raw = bytearray(2 * len(data))
     for i, b in enumerate(data):
-        raw_pal[2 * i] = b >> 4
-        raw_pal[2 * i + 1] = b & 0x0F
+        raw[2 * i] = b >> 4
+        raw[2 * i + 1] = b & 0x0F
+    return raw
+
+def save_4bit_image(data, size, palette, dest):
+    raw_pal = expand_4bit(data)
     save_8bit_image(raw_pal, size, palette, dest)
 
+def print_4bit_image(data, size):
+    raw_pal = expand_4bit(data)
+    ind = 0
+    for j in xrange(size[1]):
+        for i in xrange(size[0]):
+            print "%02x" % raw_pal[ind],
+            ind += 1
+        print
 
 def save_8bit_image(data, size, palette, dest):
     if size == (320, 11250):
@@ -33,6 +45,36 @@ def save_8bit_image(data, size, palette, dest):
     img = pygame.image.fromstring(str(raw_rgb), size, "RGB")
     pygame.image.save(img, dest)
 
+def save_RGB16_image(data, size, dest):
+    data = expand_4bit(data)
+    raw_rgb = bytearray(3 * size[0] * size[1])
+    print size, len(data), len(raw_rgb)
+    for i in xrange(size[0]):
+        for j in xrange(size[1]):
+            di = i % 2
+            dj = j % 2
+            order = [0, 0, 1, 1]
+            order = [2, 2, 3, 3]
+            order = [0, 2, 1, 3]
+
+            linelen = 2 * size[0]
+            hpos1 = order[di + 2 * dj] * (size[0] // 2)
+            hpos2 = (i // 2)
+            vdist = linelen * size[1] // 2
+            vpos = linelen * j // 2
+            hpos = hpos1 + hpos2
+            b = data[vdist * 0 + hpos + vpos]
+            g = data[vdist * 1 + hpos + vpos]
+            r = data[vdist * 2 + hpos + vpos]
+            f = data[vdist * 3 + hpos + vpos]
+            def cm(c, f):
+                return (c + f) * 8
+            raw_rgb[(i + size[0] * j) * 3:(i + size[0] * j + 1) * 3] = [cm(r, f), cm(g, f), cm(b, f)]
+
+    img = pygame.image.fromstring(str(raw_rgb), size, "RGB")
+    pygame.image.save(img, dest)
+
+
 def palette_grey16():
     pal = []
     for i in xrange(16):
@@ -44,6 +86,56 @@ def palette_grey256():
     for i in xrange(256):
         pal.append(bytearray([i, i, i]))
     return pal
+
+def palette_ega16():
+    ega_pal = palette_ega256()
+    pal = [ega_pal[0],
+           ega_pal[1],
+           ega_pal[2],
+           ega_pal[3],
+           ega_pal[4],
+           ega_pal[5],
+           ega_pal[20],
+           ega_pal[7],
+           ega_pal[56],
+           ega_pal[57],
+           ega_pal[58],
+           ega_pal[59],
+           ega_pal[60],
+           ega_pal[61],
+           ega_pal[62],
+           ega_pal[63],
+           ]
+    return pal
+
+def palette_ega256():
+    ega_pal = []
+    bit = lambda b, i: (b >> i) & 1
+    for i in range(64):
+        b = 0x55 * (2 * bit(i, 0) + bit(i, 3))
+        g = 0x55 * (2 * bit(i, 1) + bit(i, 4))
+        r = 0x55 * (2 * bit(i, 2) + bit(i, 5))
+        ega_pal.append(bytearray([r, g, b]))
+    return ega_pal
+
+
+
+def read_palette(name, path):
+    # does not work
+    data = btfile.load_file(name, path)
+    pal = []
+    for i in xrange(256):
+        b, g, r, w = data[4 * i:4 * (i + 1)]
+        r, g, b, w = data[4 * i:4 * (i + 1)]
+
+        def cm(c, f):
+#            return min(c * f, 255)
+            return min((c + f) * 8, 255)
+        print([cm(r, w), cm(g, w), cm(b, w)])
+        pal.append(bytearray([cm(r, w), cm(g, w), cm(b, w)]))
+    return pal
+
+
 
 def view_size():
     return (112, 88)
--- src/testing.py
+++ src/testing.py
@@ -14,61 +14,62 @@ data = btfile.load_indexed_file("levs", msdos_bt1_path)
 
 # images
 
-pal = btimage.palette_grey16()
-pal256 = btimage.palette_grey256()
+pal = btimage.palette_ega16()
+pal256 = btimage.palette_ega256()
 vsize = btimage.view_size()
 
-for i in xrange(4):
-    infile = "b%d.huf" % i
-    outfile = "b%d.png" % i
-    btfile.show_compressed_file_info("b0.huf", msdos_bt1_path)
-    data = btfile.load_compressed_file(infile, msdos_bt1_path)
-    btimage.save_4bit_image(data, vsize, pal, outfile)
+for i in xrange(0, 4):
+    infilename = "b%d.huf" % i
+    outfilename = "b%d.png" % i
+    btfile.show_compressed_file_info(infilename, msdos_bt1_path)
+    data = btfile.load_compressed_file(infilename, msdos_bt1_path)
+    btimage.save_4bit_image(data, vsize, pal, outfilename)
 
-if False:
+if True:
     btfile.show_compressed_file_info("bardscr", msdos_bt1_path)
     data = btfile.load_compressed_file("bardscr", msdos_bt1_path)
-    btimage.save_4bit_image(data, (320, 250), pal, "bardscr.png")
+    btimage.save_RGB16_image(data, (160, 100), "bardscr.png")
 
     btfile.show_compressed_file_info("bardtit", msdos_bt1_path)
     data = btfile.load_compressed_file("bardtit", msdos_bt1_path)
-    btimage.save_4bit_image(data, (320, 250), pal, "bardtit.png")
+    btimage.save_RGB16_image(data, (160, 100), "bardtit.png")
 
 if False:
     btfile.show_compressed_file_info("rgb_tit", msdos_bt1_path)
     data = btfile.load_compressed_file("rgb_tit", msdos_bt1_path)
     btimage.save_4bit_image(data, (256, 128), pal, "rgb_tit.png")
     rgbdata = data
-if True:
+if False:
     btfile.show_indexed_file_info("bigpic", msdos_bt1_path)
     data = btfile.load_indexed_file("bigpic", msdos_bt1_path)
     btimage.save_4bit_image(data[0], (4 * 29, 79), pal, "bigpic0.png")
 
 
-btfile.show_compressed_file_info("dpics0", msdos_bt1_path)
-data0 = btfile.load_compressed_file("dpics0", msdos_bt1_path)
-btfile.show_compressed_file_info("dpics1", msdos_bt1_path)
-data1 = btfile.load_compressed_file("dpics1", msdos_bt1_path)
-btfile.show_compressed_file_info("dpics2", msdos_bt1_path)
-data2 = btfile.load_compressed_file("dpics2", msdos_bt1_path)
+if False:
+    btfile.show_compressed_file_info("dpics0", msdos_bt1_path)
+    data0 = btfile.load_compressed_file("dpics0", msdos_bt1_path)
+    btfile.show_compressed_file_info("dpics1", msdos_bt1_path)
+    data1 = btfile.load_compressed_file("dpics1", msdos_bt1_path)
+    btfile.show_compressed_file_info("dpics2", msdos_bt1_path)
+    data2 = btfile.load_compressed_file("dpics2", msdos_bt1_path)
 
 
-btimage.save_4bit_image(data0, (72 * 2, 307), pal, "dpics0.png")
-btimage.save_4bit_image(data1, (72 * 2, 307), pal, "dpics1.png")
-btimage.save_4bit_image(data2, (72 * 2, 307), pal, "dpics2.png")
+    btimage.save_4bit_image(data0, (72 * 2, 307), pal, "dpics0.png")
+    btimage.save_4bit_image(data1, (72 * 2, 307), pal, "dpics1.png")
+    btimage.save_4bit_image(data2, (72 * 2, 307), pal, "dpics2.png")
 
 
-foo = []
-for i in xrange(48):
-    foo += data0[i::48]
-foo = data0[13:] + data0[:13]
-btimage.save_8bit_image(data0, (72, 307), pal256, "dpics0b.png")
-btimage.save_8bit_image(foo, (72, 307), pal256, "dpics0c.png")
+    foo = []
+    for i in xrange(48):
+        foo += data0[i::48]
+    foo = data0[13:] + data0[:13]
+    btimage.save_8bit_image(data0, (72, 307), pal256, "dpics0b.png")
+    btimage.save_8bit_image(foo, (72, 307), pal256, "dpics0c.png")
 
 
-#screen = btfile.rle_decode(data2)
-#xxx = bytearray(sum(screen, []))
-#btimage.save_4bit_image(xxx, (128 * 2, 160), pal, "dpics2.png")
+    #screen = btfile.rle_decode(data2)
+    #xxx = bytearray(sum(screen, []))
+    #btimage.save_4bit_image(xxx, (128 * 2, 160), pal, "dpics2.png")
 
 
 # 47 image of inside adv guild
