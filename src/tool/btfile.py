0d2be8052ee197f509a187e984ed10cf49b4937a
Desmet Irkm <bt@zandere.de>
2011-12-07 14:22:57 +0100

    much better tool now

--- src/tool/btfile.py
+++ src/tool/btfile.py
@@ -5,12 +5,14 @@ LITTLE_ENDIAN = 0
 BIG_ENDIAN = 1
 GUESS = 2
 
-def load_file(name, path="."):
+def load_file(name, path=".", hex=False):
     """Load a file and return as raw bytearray"""
     filename = os.path.join(path, name)
     filename = filename.replace('/', os.path.sep)
     with open(filename, "rb") as f:
         byte_arr = bytearray(f.read())
+    if hex:
+        byte_arr = hex_to_bin(byte_arr)
     return byte_arr
 
 def read_long(byte_arr, offset, endian=GUESS):
@@ -31,7 +33,7 @@ def load_indexed_file(name, path=".", endian=GUESS):
 
     def get_chunk(byte_arr, index, num_chunks):
         start = read_long(byte_arr, index * 4, endian)
-        if index<num_chunks-1:
+        if index < num_chunks - 1:
             end = read_long(byte_arr, index * 4 + 4, endian)
         else:
             end = len(byte_arr)
@@ -87,9 +89,9 @@ def print_htree(htree, count=None):
     data)."""
     for (val, bits) in iterate_htree(htree):
         if count:
-            print  "%02d %s: %02x (%d)" %  (len(bits), bits, val, count[val])
+            print  "%02d %s: %02x (%d)" % (len(bits), bits, val, count[val])
         else:
-            print  "%s: %02x" %  (bits, val)
+            print  "%s: %02x" % (bits, val)
 
 # Part 4: Decoding the data using the Huffman trees
 def decode(bit_seq, htree, decomp_size):
@@ -120,31 +122,31 @@ def decode_chunk(byte_arr, endians=(GUESS, GUESS)):
 def count_data(data):
     count = [0] * 256
     for b in data:
-        count[b]+=1
+        count[b] += 1
     return count
 
 def build_htree(data, count=None):
     if not count:
         count = count_data(data)
-    ht = sorted((count[i], i) for i in xrange(256) if count[i]>0)
-    while len(ht)>1:
-        sub_cnt=ht[0][0]+ht[1][0]
-        sub_ht=[ht[0][1], ht[1][1]]
-        ht[0:2]=[(sub_cnt, sub_ht)]
-        ht=sorted(ht)
+    ht = sorted((count[i], i) for i in xrange(256) if count[i] > 0)
+    while len(ht) > 1:
+        sub_cnt = ht[0][0] + ht[1][0]
+        sub_ht = [ht[0][1], ht[1][1]]
+        ht[0:2] = [(sub_cnt, sub_ht)]
+        ht = sorted(ht)
     return ht[0][1]
 
 
 # Part 6: Encoding the data and writing into a buffer
 def write_long(byte_arr, value, offset=0, endian=BIG_ENDIAN):
     """Write a long value into a byte array using a given endian"""
-    while len(byte_arr)<offset+4:
+    while len(byte_arr) < offset + 4:
         byte_arr.append(0)
     if endian == BIG_ENDIAN:
         offset += 3
-        dir=-1
+        dir = -1
     else:
-        dir=1
+        dir = 1
     for i in xrange(4):
         byte_arr[offset] = value & 0xFF
         value >>= 8
@@ -153,20 +155,20 @@ def write_long(byte_arr, value, offset=0, endian=BIG_ENDIAN):
 class BitWriter(object):
     def __init__(self, byte_arr):
         self.byte_arr = byte_arr
-        self.total=0
-        self.bits=0
-        self.byte=0
+        self.total = 0
+        self.bits = 0
+        self.byte = 0
     def write_bit(self, bit):
-        self.total+=1
-        self.bits+=1
-        self.byte = self.byte<<1 | (1 if bit else 0)
-        if self.bits==8:
+        self.total += 1
+        self.bits += 1
+        self.byte = self.byte << 1 | (1 if bit else 0)
+        if self.bits == 8:
             self.flush()
     def flush(self):
         if self.bits:
-            self.byte_arr.append(self.byte<<(8-self.bits))
-        self.byte=0
-        self.bits=0
+            self.byte_arr.append(self.byte << (8 - self.bits))
+        self.byte = 0
+        self.bits = 0
         return self.total
 
 def write_htree(bit_writer, htree):
@@ -191,7 +193,7 @@ def encode_data(bit_writer, htree, data):
 
     for b in data:
         for c in htable[b]:
-            bit_writer.write_bit(c=="1")
+            bit_writer.write_bit(c == "1")
 
 def encode_chunk(data, htree=None, endians=(BIG_ENDIAN, BIG_ENDIAN)):
     """Decode a chunk of Huffman encoded data"""
@@ -201,7 +203,7 @@ def encode_chunk(data, htree=None, endians=(BIG_ENDIAN, BIG_ENDIAN)):
     write_long(byte_arr, decomp_size, 0)
     write_long(byte_arr, comp_size, 4)
 
-    if htree==None:
+    if htree == None:
         htree = build_htree(data)
     bit_writer = BitWriter(byte_arr)
     write_htree(bit_writer, htree)
@@ -210,26 +212,99 @@ def encode_chunk(data, htree=None, endians=(BIG_ENDIAN, BIG_ENDIAN)):
     write_long(byte_arr, comp_size, 4)
     return byte_arr
 
-# 
-def write_file(name, byte_arr, path="."):
+# Part 7: Writing the files
+def write_file(name, byte_arr, path=".", hex=False):
     """Write data to a file."""
     filename = os.path.join(path, name)
     filename = filename.replace('/', os.path.sep)
+    if hex:
+        byte_arr = bin_to_hex(byte_arr)
     with open(filename, "wb") as f:
         f.write(byte_arr)
 
 def write_indexed_file(name, byte_arrs, path=".", endian=BIG_ENDIAN):
     index = bytearray()
-    offset = len(byte_arrs)*4
-    indexpos=0
+    offset = len(byte_arrs) * 4
+    indexpos = 0
     for byte_arr in byte_arrs:
         write_long(index, offset, indexpos)
         offset += len(byte_arr)
         indexpos += 4
     byte_arrs.insert(0, index)
-    
+
     byte_arr = bytearray().join(byte_arrs)
     write_file(name, byte_arr, path)
+
+# Part 9: Some tools
+def expand(infile, outfile, hex=False):
+    byte_arr = load_file(infile)
+    data = decode_chunk(byte_arr)
+    write_file(outfile, data, hex=hex)
+
+def compress(infile, outfile, hex=False):
+    data = load_file(infile, hex=hex)
+    byte_arr = encode_chunk(data)
+    write_file(outfile, byte_arr)
+
+def expand_indexed(infile, outfile_pattern, hex=False):
+    byte_arrs = load_indexed_file(infile)
+    for i in xrange(len(byte_arrs)):
+        data = decode_chunk(byte_arrs[i])
+        write_file(outfile_pattern % i, data, hex=hex)
+
+def compress_indexed(infile_pattern, outfile, hex=False):
+    from glob import glob
+    byte_arrs = []
+    for filename in sorted(glob(infile_pattern)):
+        data = load_file(filename, hex=hex)
+        byte_arrs.append(encode_chunk(data))
+    write_indexed_file(outfile, byte_arrs)
+
+#
+def bin_to_hex(byte_arr):
+    out = bytearray()
+    for i, b in enumerate(byte_arr):
+        out.extend("%02X " % b)
+        if i % 16 == 0xF:
+            out.extend("\n")
+    return out
+
+
+def hex_to_bin(byte_arr):
+    byte_arr = byte_arr.translate(bytearray(range(256)), " \n\r\t")
+    out = bytearray().fromhex(str(byte_arr))
+    return out
+
+#
+
+def get_identity_htree():
+    return build_htree(data=bytearray(xrange(256)))
+
+def identity_recompress(filename):
+    id_htree = get_identity_htree()
+    byte_arr = load_file(filename)
+    data = decode_chunk(byte_arr)
+    byte_arr = encode_chunk(data, htree=id_htree)
+    write_file(filename, byte_arr)
+
+def identity_recompress_indexed(filename):
+    id_htree = get_identity_htree()
+    byte_arrs = load_indexed_file(filename)
+    for i in xrange(len(byte_arrs)):
+        data = decode_chunk(byte_arrs[i])
+        byte_arrs[i] = encode_chunk(data, htree=id_htree)
+    write_indexed_file(filename, byte_arrs)
+
+
+
+# Some other stuff 
+
+def bit_length_compressed(data, htree):
+    count = count_data(data)
+    bl = 0
+    for (val, bits) in iterate_htree(htree):
+        bl += count[val] * len(bits)
+    return bl
 
 
 
---
+++ src/tool/bttool.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+import os
+import sys
+
+import btfile
+
+opt_args = [ "expand", "compress", "identity", "hex"]
+val_args = [ "fileid"]
+
+suppfiles = [
+             ("level", "levs", True),
+             ("pics", "bigpic", True),
+             ("guild?", "47", True),
+             ("citymap", "city.pat", False),
+             ("streets", "city.nam", False),
+             ("house1", "b0.huf", False),
+             ("house2", "b1.huf", False),
+             ("house3", "b2.huf", False),
+             ("house4", "b3.huf", False),
+             ("dungtype1", "dpics0", False),
+             ("dungtype2", "dpics1", False),
+             ("dungtype3", "dpics2", False),
+             ("intro", "bardtit", False),
+             ("gamescr", "bardpic", False),
+             ]
+
+def help_args(err=0, arg=""):
+    print "USAGE: bttool {-expand|-compress|-identity} [-hex] -fileid=<file>"
+    print " Specify one of:"
+    print "  -expand: uncompress BT file to binary or hex format "
+    print "  -compress: compress binary or hex file to BT file"
+    print "  -identity: recompress BT file to use identity huffman tree"
+    print
+    print "  -fileid: file to work on (use filename or identifier)"
+    print "         see below for a list of supported files"
+    print "  -hex: expand as hex file (easier for humans to edit, worse for programs)"
+    print
+    print "  Supported Fileids:"
+    for ident, filename, indexed in suppfiles:
+        if indexed:
+            print "     %s = %s (indexed)" % (ident, filename)
+        else:
+            print "     %s = %s" % (ident, filename)
+
+
+    if err == 1:
+        print "ERR: Exactly one of [" + " ".join(opt_args[:3]) + "] must be specified"
+    elif err == 2:
+        print "ERR: identity and hex together makes no sense"
+    elif err == 3:
+        print "ERR:unrecognised argument %s" % arg
+    elif err == 4:
+        print "ERR:unrecognised fileid: '%s'" % arg
+    sys.exit(err)
+
+
+if len(sys.argv) <= 1:
+    help_args()
+
+hex = False
+expand = False
+identity = False
+compress = False
+fileid = None
+for arg in sys.argv[1:]:
+    import __main__ as main_mod
+    arg = arg.lstrip("-")
+    args = map(str.strip, arg.split("="))
+    if arg in opt_args:
+        main_mod.__dict__[arg] = True
+    elif args[0] in val_args:
+        main_mod.__dict__[args[0]] = args[1]
+    else:
+        help_args(3, arg)
+
+found = False
+for (name, basename, indexed) in suppfiles:
+    if fileid in [name, basename]:
+        found = True
+        break
+if not found:
+    help_args(4, fileid)
+
+if hex:
+    extension = "hex"
+else:
+    extension = "bin"
+
+if expand:
+    if compress or identity:
+        help_args(1)
+    infilename = basename
+    if indexed:
+        outfilepat = "%s-%%02d.%s" % (basename, extension)
+        btfile.expand_indexed(infilename, outfilepat, hex=hex)
+    else:
+        outfilename = "%s.%s" % (basename, extension)
+        btfile.expand(infilename, outfilename, hex=hex)
+elif compress:
+    if identity:
+        help_args(1)
+    outfilename = basename
+    if indexed:
+        infileglob = "%s-*.%s" % (basename, extension)
+        btfile.compress_indexed(infileglob, outfilename, hex=hex)
+    else:
+        infilename = "%s.%s" % (basename, extension)
+        btfile.compress(infilename, outfilename, hex=hex)
+elif identity:
+    if hex:
+        help_args(2)
+    if indexed:
+        btfile.identity_recompress_indexed(basename)
+    else:
+        btfile.identity_recompress(basename)
+else:
+    help_args(1)
+
